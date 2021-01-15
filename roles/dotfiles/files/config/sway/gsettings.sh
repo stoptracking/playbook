aef2f39abc68172d78d83d0e580e46961d5a0d23
Andrew Savchenko <andrew@savchenko.net>
2021-01-15 16:31:48 +1030

    Cursor size / Sway
    Former-commit-id: cf40aaa50d18ffa4e2d2f1c48e81dde8ef5b7697

--- roles/dotfiles/files/config/sway/gsettings.sh
+++ roles/dotfiles/files/config/sway/gsettings.sh
@@ -5,6 +5,7 @@ if [ ! -f "$config" ]; then exit 1; fi
 
 gnome_schema="org.gnome.desktop.interface"
 
+cursor_size="$(grep 'gtk-cursor-theme-size' "$config" | cut -d'=' -f2)"
 cursor_blink="$(grep 'gtk-cursor-blink' "$config" | cut -d'=' -f2)"
 cursor_theme="$(grep 'gtk-cursor-theme-name' "$config" | cut -d'=' -f2)"
 enable_animations="$(grep 'gtk-enable-animations' "$config" | cut -d'=' -f2)"
@@ -13,6 +14,7 @@ gtk_theme="$(grep 'gtk-theme-name' "$config" | cut -d'=' -f2)"
 icon_theme="$(grep 'gtk-icon-theme-name' "$config" | cut -d'=' -f2)"
 overlay_scrolling="$(grep 'gtk-overlay-scrolling' "$config" | cut -d'=' -f2)"
 
+gsettings set "$gnome_schema" cursor-size "$cursor_size"
 gsettings set "$gnome_schema" cursor-blink "$cursor_blink"
 gsettings set "$gnome_schema" cursor-theme "$cursor_theme"
 gsettings set "$gnome_schema" enable-animations "$enable_animations"
