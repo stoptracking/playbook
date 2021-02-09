56ee95e9794fdee51aec25ff8dba52c5667cd40f
Andrew Savchenko <andrew@savchenko.net>
2021-02-09 13:24:02 +1030

    Fix screenshot mover

--- roles/dotfiles/files/bin/screenshots.sh
+++ roles/dotfiles/files/bin/screenshots.sh
@@ -2,16 +2,16 @@
 
 # Based on https://stackoverflow.com/a/42266990
 today=$(date +%d-%b)
-month=$(date +%b)
            # 08-Feb_14-57-40.png
 for fname in ??-???_??-??-??.png; do
   fname_date=${fname:0:6}
-  fdest=$PWD/$month/$fname_date
+  fname_month=${fname:3:3}
+  fdest=$PWD/$fname_month/$fname_date
   [ $today = $fname_date ] && continue;
   [ ! -d $fdest ] && mkdir -p $fdest
   if [ ! -d $fdest ]; then
     echo "Error: Can not create directory $fdest"
     break
   fi
-  mv $fname $month/$fname_date
+  mv $fname $fname_month/$fname_date
 done
