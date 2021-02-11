fc44949b30fe5127dfada5c702d76d2f80030573
Andrew Savchenko <andrew@savchenko.net>
2021-02-11 21:47:07 +1030

    Update `alsaswitcher` to restart mpd

--- roles/dotfiles/files/config/sway/alsaswitcher.sh
+++ roles/dotfiles/files/config/sway/alsaswitcher.sh
@@ -18,6 +18,8 @@ if [ $cards_count -lt 10 ]; then
     grep -E -q 'device\s[0-9]' $asoundrc && sed -r -i "s/(device\s)[0-9]/\1$device/g" $asoundrc
     if [ $? -eq 0 ]; then
       sudo alsaucm reload
+      pkill --full --exact mpd
+      mpd
     fi
     if [[ -n "$TERM" && -n "$COLORTERM" ]]; then
       echo "Switching to card #$card and device #$device ($description)"
